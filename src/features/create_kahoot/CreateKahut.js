777182497938d5cea3e4a8eae43afda748a9a623
Nguyễn Văn Lâm <60528316+NguyenLam05200@users.noreply.github.com>
2022-06-18 15:57:04 +0700

    layout createkahut page

--- src/features/create_kahoot/CreateKahut.js
+++ src/features/create_kahoot/CreateKahut.js
@@ -7,8 +7,30 @@ const CreateKahut = () => {
   return (
     <Paper elevation={0}>
       <Navbar />
-      Hello from create/kahut
-    </Paper>
+      <Grid container m='auto' p='auto' >
+        <Box
+          width='15%'
+          boxShadow={12}
+          sx={{ justifyContent: 'center', display: 'flex', py: 3 }}
+        >
+          slide
+        </Box>
+        <Box
+          width='65%'
+          height='88vh'
+          sx={{ justifyContent: 'center', display: 'flex', py: 3 }}
+        >
+          create question
+        </Box>
+        <Box
+          width='20%'
+          boxShadow={12}
+          sx={{ justifyContent: 'center', display: 'flex', py: 3 }}
+        >
+          chooser
+        </Box>
+      </Grid>
+    </Paper >
   );
 };
 
