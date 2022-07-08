26bbe657841b86a4d487a6adc988e7ea796f5293
HuyDoan198 <49835032+HuyDoan198@users.noreply.github.com>
2022-07-09 00:00:37 +0700

    a

--- src/features/kahoot detail/kahoot_detail.js
+++ src/features/kahoot detail/kahoot_detail.js
@@ -5,6 +5,7 @@ import Navbar from "../user/Navbar";
 import { selectAllQuiz, fetchQuiz } from "../quizSlice/quizSlice";
 import { useParams } from "react-router-dom";
 
+
 const KahootDetail = function ({ match }) {
   const [showAnswer, setShowAnswer] = useState(false);
   const clickShowAnser = () => {
@@ -34,7 +35,203 @@ const KahootDetail = function ({ match }) {
     const ques_number = 1;
     content = Quiz.questions.map((item) => {
       //map datat
-      <div></div>;
+      <div className="mb-3">
+        <div
+          style={{
+            backgroundColor: "#fff",
+            border: "none",
+            padding: 5,
+          }}
+        >
+          <div className="row">
+            <div className="col-sm-9">
+              <h5>{ques_number} - Quiz</h5>
+              <span style={{ fontWeight: "bold" }}>{item.text}</span>
+            </div>
+            <div className="col-sm-3">
+              <img style={{ width: "100%" }} src={item.img} alt />
+              <span
+                style={{
+                  position: "absolute",
+                  marginTop: 60,
+                  marginLeft: "-50px",
+                  color: "white",
+                }}
+              >
+                {item.time} sec
+              </span>
+            </div>
+          </div>
+        </div>
+        {showAnswer && (
+          <div>
+            {item.ans.map((ques) => {
+              {
+                ques_number === 1 && (
+                  <div
+                    style={{
+                      border: "solid 1px #f5f5f5",
+                      backgroundColor: "#fff",
+                    }}
+                  >
+                    <div
+                      className="row d-flex justify-content-between"
+                      style={{ margin: 0 }}
+                    >
+                      <div>
+                        <i
+                          className="ml-2 mr-1 fa fa-eercast"
+                          aria-hidden="true"
+                          style={{ color: "red" }}
+                        />
+                        <span>{ques.text}</span>
+                      </div>
+
+                      {ques.isRight ? (
+                        <i
+                          style={{
+                            color: "rgb(0, 255, 13)",
+                            marginRight: 5,
+                          }}
+                          className="fa fa-check"
+                          aria-hidden="true"
+                        ></i>
+                      ) : (
+                        <i
+                          style={{ color: "red", marginRight: 5 }}
+                          className="fa fa-times"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </div>
+                  </div>
+                );
+              }
+              {
+                ques_number === 2 && (
+                  <div
+                    style={{
+                      border: "solid 1px #f5f5f5",
+                      backgroundColor: "#fff",
+                    }}
+                  >
+                    <div
+                      className="row d-flex justify-content-between"
+                      style={{ margin: 0 }}
+                    >
+                      <div>
+                        <i
+                          className="ml-2 mr-1 fa fa-superpowers"
+                          aria-hidden="true"
+                          style={{ color: "rgb(0, 255, 76)" }}
+                        />
+                        <span>{ques.text}</span>
+                      </div>
+                      {ques.isRight ? (
+                        <i
+                          style={{
+                            color: "rgb(0, 255, 13)",
+                            marginRight: 5,
+                          }}
+                          className="fa fa-check"
+                          aria-hidden="true"
+                        ></i>
+                      ) : (
+                        <i
+                          style={{ color: "red", marginRight: 5 }}
+                          className="fa fa-times"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </div>
+                  </div>
+                );
+              }
+
+              {
+                ques_number === 3 && (
+                  <div
+                    style={{
+                      border: "solid 1px #f5f5f5",
+                      backgroundColor: "#fff",
+                    }}
+                  >
+                    <div
+                      className="row d-flex justify-content-between"
+                      style={{ margin: 0 }}
+                    >
+                      <div>
+                        <i
+                          className="ml-2 mr-1 fa fa-grav"
+                          aria-hidden="true"
+                          style={{ color: "rgb(0, 238, 255)" }}
+                        />
+                        <span>{ques.text}</span>
+                      </div>
+                      {ques.isRight ? (
+                        <i
+                          style={{
+                            color: "rgb(0, 255, 13)",
+                            marginRight: 5,
+                          }}
+                          className="fa fa-check"
+                          aria-hidden="true"
+                        ></i>
+                      ) : (
+                        <i
+                          style={{ color: "red", marginRight: 5 }}
+                          className="fa fa-times"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </div>
+                  </div>
+                );
+              }
+              {
+                ques_number === 4 && (
+                  <div
+                    style={{
+                      border: "solid 1px #f5f5f5",
+                      backgroundColor: "#fff",
+                    }}
+                  >
+                    <div
+                      className="row d-flex justify-content-between"
+                      style={{ margin: 0 }}
+                    >
+                      <div>
+                        <i
+                          className="ml-1 mr-1 fa fa-ravelry"
+                          aria-hidden="true"
+                          style={{ color: "rgb(0, 255, 42)" }}
+                        />
+                        <span>{ques.text}</span>
+                      </div>
+                      {ques.isRight ? (
+                        <i
+                          style={{
+                            color: "rgb(0, 255, 13)",
+                            marginRight: 5,
+                          }}
+                          className="fa fa-check"
+                          aria-hidden="true"
+                        ></i>
+                      ) : (
+                        <i
+                          style={{ color: "red", marginRight: 5 }}
+                          className="fa fa-times"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        )}
+      </div>;
       ques_number++;
     });
   } else if (postStatus === "failed") {
