45a9a7236d1c56db70575a2056e55e7f706b968b
HuyDoan198 <49835032+HuyDoan198@users.noreply.github.com>
2022-07-09 00:28:58 +0700

    fix

--- src/features/quizSlice/quizSlice.js
+++ src/features/quizSlice/quizSlice.js
@@ -6,7 +6,7 @@ export const fetchQuiz = createAsyncThunk(
   "quiz/getListQuiz",
   async (roomID, thunkAPI) => {
     const Token =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imh1eWRvYW5AZ21haWwuY29tIiwidGVzdF9maWVsZCI6InRlc3QiLCJleHRyYSI6e30sInNjb3BlIjpbInNjb3BlLmFkbWluIl0sImlhdCI6MTY1NzIxODM4OCwiZXhwIjoxNjU3MjIxOTg4LCJzdWIiOiJodXlkb2FuQGdtYWlsLmNvbSJ9.N7GWoR4VA9azQ7SHee85308r9JJxwp2F8eTkUdlWKDo";
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imh1eWRvYW5AZ21haWwuY29tIiwidGVzdF9maWVsZCI6InRlc3QiLCJleHRyYSI6e30sInNjb3BlIjpbInNjb3BlLmFkbWluIl0sImlhdCI6MTY1NzMwMDY2NiwiZXhwIjoxNjU3MzA0MjY2LCJzdWIiOiJodXlkb2FuQGdtYWlsLmNvbSJ9.PR-Fjh26rsFbFZ9vysP9PKwiyR_PeFGq7pmbtFvnrLM";
     // axios
     //   .get(`//157.245.147.239:80/v1/api/admin/quiz/${roomID}`, {
     //     headers: {
